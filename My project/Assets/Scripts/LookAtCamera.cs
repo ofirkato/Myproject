b667ca641dfa10bb42b75cae84eb70273cf9189f
ofirkato <ofirkatorza@gmail.com>
2024-01-28 19:33:45 +0200

    after trash counter

---
+++ My project/Assets/Scripts/LookAtCamera.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour
+{
+    private enum Mode
+    {
+        LookAt,
+        LookAtInverted,
+        CameraForward,
+        CameraForwardInverted,
+    }
+
+    [SerializeField] private Mode mode;
+    
+
+ private void LateUpdate() { 
+         transform.LookAt(Camera.main.transform);
+       }
+}
